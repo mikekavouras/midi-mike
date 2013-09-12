66aa1160c40edf1b61bb11b889d235adc48d7bd2
mikekavouras <mike@wantful.com>
2013-09-12 14:15:26 -0400

    firsties

---
+++ script.js
@@ -0,0 +1,188 @@
+var NOTES = { "C0" : { "frequency" : "16.35", "wavelength" : "2100" }, "C#0" : { "frequency" : "17.32", "wavelength" : "1990" }, "Db0" : { "frequency" : "17.32", "wavelength" : "1990" }, "D0" : { "frequency" : "18.35", "wavelength" : "1870" }, "D#0" : { "frequency" : "19.45", "wavelength" : "1770" }, "Eb0" : { "frequency" : "19.45", "wavelength" : "1770" }, "E0" : { "frequency" : "20.60", "wavelength" : "1670" }, "F0" : { "frequency" : "21.83", "wavelength" : "1580" }, "F#0" : { "frequency" : "23.12", "wavelength" : "1490" }, "Gb0" : { "frequency" : "23.12", "wavelength" : "1490" }, "G0" : { "frequency" : "24.50", "wavelength" : "1400" }, "G#0" : { "frequency" : "25.96", "wavelength" : "1320" }, "Ab0" : { "frequency" : "25.96", "wavelength" : "1320" }, "A0" : { "frequency" : "27.50", "wavelength" : "1250" }, "A#0" : { "frequency" : "29.14", "wavelength" : "1180" }, "Bb0" : { "frequency" : "29.14", "wavelength" : "1180" }, "B0" : { "frequency" : "30.87", "wavelength" : "1110" }, "C1" : { "frequency" : "32.70", "wavelength" : "1050" }, "C#1" : { "frequency" : "34.65", "wavelength" : "996" }, "Db1" : { "frequency" : "34.65", "wavelength" : "996" }, "D1" : { "frequency" : "36.71", "wavelength" : "940" }, "D#1" : { "frequency" : "38.89", "wavelength" : "887" }, "Eb1" : { "frequency" : "38.89", "wavelength" : "887" }, "E1" : { "frequency" : "41.20", "wavelength" : "837" }, "F1" : { "frequency" : "43.65", "wavelength" : "790" }, "F#1" : { "frequency" : "46.25", "wavelength" : "746" }, "G#1" : { "frequency" : "46.25", "wavelength" : "746" }, "G1" : { "frequency" : "49.00", "wavelength" : "704" }, "G#1" : { "frequency" : "51.91", "wavelength" : "665" }, "Ab1" : { "frequency" : "51.91", "wavelength" : "665" }, "A1" : { "frequency" : "55.00", "wavelength" : "627" }, "A#1" : { "frequency" : "58.27", "wavelength" : "592" }, "Bb1" : { "frequency" : "58.27", "wavelength" : "592" }, "B1" : { "frequency" : "61.74", "wavelength" : "559" }, "C2" : { "frequency" : "65.41", "wavelength" : "527" }, "C#2" : { "frequency" : "69.30", "wavelength" : "498" }, "Db2" : { "frequency" : "69.30", "wavelength" : "498" }, "D2" : { "frequency" : "73.42", "wavelength" : "470" }, "D#2" : { "frequency" : "77.78", "wavelength" : "444" }, "Eb2" : { "frequency" : "77.78", "wavelength" : "444" }, "E2" : { "frequency" : "82.41", "wavelength" : "419" }, "F2" : { "frequency" : "87.31", "wavelength" : "395" }, "F#2" : { "frequency" : "92.50", "wavelength" : "373" }, "Gb2" : { "frequency" : "92.50", "wavelength" : "373" }, "G2" : { "frequency" : "98.00", "wavelength" : "352" }, "G#2" : { "frequency" : "103.83", "wavelength" : "332" }, "Ab2" : { "frequency" : "103.83", "wavelength" : "332" }, "A2" : { "frequency" : "110.00", "wavelength" : "314" }, "A#2" : { "frequency" : "116.54", "wavelength" : "296" }, "Bb2" : { "frequency" : "116.54", "wavelength" : "296" }, "B2" : { "frequency" : "123.47", "wavelength" : "279" }, "C3" : { "frequency" : "130.81", "wavelength" : "264" }, "C#3" : { "frequency" : "138.59", "wavelength" : "249" }, "Db3" : { "frequency" : "138.59", "wavelength" : "249" }, "D3" : { "frequency" : "146.83", "wavelength" : "235" }, "D#3" : { "frequency" : "155.56", "wavelength" : "222" }, "Eb3" : { "frequency" : "155.56", "wavelength" : "222" }, "E3" : { "frequency" : "164.81", "wavelength" : "209" }, "F3" : { "frequency" : "174.61", "wavelength" : "198" }, "F#3" : { "frequency" : "185.00", "wavelength" : "186" }, "Gb3" : { "frequency" : "185.00", "wavelength" : "186" }, "G3" : { "frequency" : "196.00", "wavelength" : "176" }, "G#3" : { "frequency" : "207.65", "wavelength" : "166" }, "Ab3" : { "frequency" : "207.65", "wavelength" : "166" }, "A3": { "frequency" : "220.00", "wavelength" : "157" }, "A#3" : { "frequency" : "233.08", "wavelength" : "148" }, "Bb3" : { "frequency" : "233.08", "wavelength" : "148" }, "B3" : { "frequency" : "246.94", "wavelength" : "140" }, "C4" : { "frequency" : "261.63", "wavelength" : "132" }, "C#4" : { "frequency" : "277.18", "wavelength" : "124" }, "Db4" : { "frequency" : "277.18", "wavelength" : "124" }, "D4" : { "frequency" : "293.66", "wavelength" : "117" }, "D#4" : { "frequency" : "311.13", "wavelength" : "111" }, "Eb4" : { "frequency" : "311.13", "wavelength" : "111" }, "E4" : { "frequency" : "329.63", "wavelength" : "105" }, "F4" : { "frequency" : "349.23", "wavelength" : "98.8" }, "F#4" : { "frequency" : "369.99", "wavelength" : "93.2" }, "Gb4" : { "frequency" : "369.99", "wavelength" : "93.2" }, "G4" : { "frequency" : "392.00", "wavelength" : "88.0" }, "G#4" : { "frequency" : "415.30", "wavelength" : "83.1" }, "Ab4" : { "frequency" : "415.30", "wavelength" : "83.1" }, "A4": { "frequency" : "440.00", "wavelength" : "78.4" }, "A#4" : { "frequency" : "466.16", "wavelength" : "74.0" }, "Bb4" : { "frequency" : "466.16", "wavelength" : "74.0" }, "B4" : { "frequency" : "493.88", "wavelength" : "69.9" }, "C5" : { "frequency" : "523.25", "wavelength" : "65.9" }, "C#5" : { "frequency" : "554.37", "wavelength" : "62.2" }, "Db5" : { "frequency" : "554.37", "wavelength" : "62.2" }, "D5" : { "frequency" : "587.33", "wavelength" : "58.7" }, "D#5" : { "frequency" : "622.25", "wavelength" : "55.4" }, "Eb5" : { "frequency" : "622.25", "wavelength" : "55.4" }, "E5" : { "frequency" : "659.26", "wavelength" : "52.3" }, "F5" : { "frequency" : "698.46", "wavelength" : "49.4" }, "F#5" : { "frequency" : "739.99", "wavelength" : "46.6" }, "Gb5" : { "frequency" : "739.99", "wavelength" : "46.6" }, "G5" : { "frequency" : "783.99", "wavelength" : "44.0" }, "G#5" : { "frequency" : "830.61", "wavelength" : "41.5" }, "Ab5" : { "frequency" : "830.61", "wavelength" : "41.5" }, "A5": { "frequency" : "880.00", "wavelength" : "39.2" }, "A#5" : { "frequency" : "932.33", "wavelength" : "37.0" }, "Bb5" : { "frequency" : "932.33", "wavelength" : "37.0" }, "B5" : { "frequency" : "987.77", "wavelength" : "34.9" }, "C6" : { "frequency" : "1046.50", "wavelength" : "33.0" } };
+
+var CHORDS = {
+  'A'     : ['A', 'C#', 'E'],
+  'Am'    : ['A', 'C', 'E'],
+  'Bb'    : ['Bb', 'D', 'F'],
+  'Bbm'   : ['Bb', 'Db', 'F'],
+  'B'     : ['B', 'D#', 'F#'],
+  'Bm'    : ['B', 'D', 'F#'],
+  'C'     : ['C', 'E', 'G'],
+  'Cm'    : ['C', 'Em', 'G'],
+  'C#'    : ['C#', 'E#', 'G#'],
+  'C#m'   : ['C#', 'E', 'G#'],
+  'C#dim' : ['C#', 'E', 'G'],
+  'Db'    : ['Db', 'F', 'Ab'],
+  'D'     : ['D', 'F#', 'A'],
+  'Dm'    : ['D', 'F', 'A'],
+  'D#'    : ['D#', 'F#', 'A#'],
+  'Eb'    : ['Eb', 'G', 'Bb'],
+  'E'     : ['E', 'G#', 'B'],
+  'Edim'  : ['E', 'G', 'Bb'],
+  'Em'    : ['E', 'G', 'B'],
+  'F'     : ['F', 'A', 'C'],
+  'Fm'    : ['F', 'Ab', 'C'],
+  'F#'    : ['F#', 'A#', 'C#'],
+  'F#m'   : ['F#', 'A', 'C#'],
+  'Gb'    : ['Gb', 'Bb', 'Db'],
+  'G'     : ['G', 'B', 'D'],
+  'Gm'    : ['G', 'Bb', 'D'],
+  'G#'    : ['G#', 'B#', 'D#'],
+  'G#dim' : ['G#', 'B', 'D']
+};
+
+var KEYS = {
+  'A' : {
+    'A' : CHORDS['A'],
+    'B' : CHORDS['Bm'],
+    'C' : CHORDS['C#m'],
+    'D' : CHORDS['D'],
+    'E' : CHORDS['E'],
+    'F' : CHORDS['F#m'],
+    'G' : CHORDS['G#dim']
+  },
+  'B' : {
+    'B' : CHORDS['B'],
+    'C' : CHORDS['C#m'],
+    'D' : CHORDS['D#m'],
+    'E' : CHORDS['E'],
+    'F' : CHORDS['F#'],
+    'G' : CHORDS['G#m'],
+    'A' : CHORDS['A#dim']
+  },
+  'C' : {
+    'C' : CHORDS['C'],
+    'D' : CHORDS['Dm'],
+    'E' : CHORDS['Em'],
+    'F' : CHORDS['F'],
+    'G' : CHORDS['G'],
+    'A' : CHORDS['Am'],
+    'B' : CHORDS['Bdim']
+  },
+  'D' : {
+    'D' : CHORDS['D'],
+    'E' : CHORDS['Em'],
+    'F' : CHORDS['F#m'],
+    'G' : CHORDS['G'],
+    'A' : CHORDS['A'],
+    'B' : CHORDS['Bm'],
+    'C' : CHORDS['C#dim']
+  },
+  'E' : {
+    'E' : CHORDS['E'],
+    'F' : CHORDS['F#m'],
+    'G' : CHORDS['G#m'],
+    'A' : CHORDS['A'],
+    'B' : CHORDS['B'],
+    'C' : CHORDS['C#m'],
+    'D' : CHORDS['D#dim']
+  },
+  'F' : {
+    'F' : CHORDS['F'],
+    'G' : CHORDS['Gm'],
+    'A' : CHORDS['Am'],
+    'B' : CHORDS['Bb'],
+    'C' : CHORDS['C'],
+    'D' : CHORDS['Dm'],
+    'E' : CHORDS['Edim']
+  },
+  'G' : {
+    'G' : CHORDS['G'],
+    'A' : CHORDS['Am'],
+    'B' : CHORDS['Bm'],
+    'C' : CHORDS['C'],
+    'D' : CHORDS['D'],
+    'E' : CHORDS['Em'],
+    'F' : CHORDS['F#dim']
+  }
+};
+
+function AudioPlayer() {
+  this.context = new webkitAudioContext();
+  this.sources = [];
+  this.volumes = [];
+  this.reverbs = [];
+
+  this.octave = 4;
+  this.key = 'C';
+
+  this.init();
+}
+
+AudioPlayer.prototype = {
+  init: function() {
+    for (var i = 0; i < 3; i++) {
+      var oscillator = this.context.createOscillator();
+      var volume = this.context.createGainNode();
+      var reverb = this.context.createConvolver();
+      oscillator.type = 3;
+      this.sources.push(oscillator);
+      this.reverbs.push(reverb);
+      this.volumes.push(volume);
+    }
+  },
+
+  play: function() {
+    for (var i = 0; i < 3; i++) {
+      this.sources[i].connect(this.volumes[i]);
+      this.volumes[i].connect(this.context.destination);
+      this.volumes[i].gain.value = 1;
+      this.sources[i].noteOn(0);
+    }
+
+    var player = this;
+    this.t = setInterval(function() {
+      for (i = 0; i < player.volumes.length; i++) {
+        player.volumes[i].gain.value -= 0.02;
+        player.sources[i].detune.value += 20;
+      }
+      if (player.volumes[0].gain.value <= 0) {
+        clearInterval(player.t);
+        for (i = 0; i < player.volumes.length; i++) {
+          player.sources[i].detune.value = 0;
+        }
+      }
+    }, 10)
+  },
+
+  pause: function() {
+    for (var i = 0; i < this.sources.length; i++) {
+      this.sources[i].disconnect(this.context.destination);
+    }
+  },
+
+  playChord: function(chord) {
+    this.pause();
+
+    chord = KEYS[this.key][chord];
+    for (var i = 0; i < chord.length; i++) {
+      var note = chord[i];
+      this.sources[i].frequency.value = NOTES[note + this.octave].frequency
+    }
+
+    this.play();
+  },
+
+  playNote: function(note) {
+
+  },
+
+  getNewSource: function() {
+  }
+};
+
+$(document).ready(function() {
+   var player = new AudioPlayer();
+
+  $('#notes button').bind('click', function(e) {
+    var note = $(this).attr('data-note');
+    player.playNote(note);
+  });
+
+  $('#chords').find('button').bind('click', function(e) {
+    var chord = $(this).attr('data-chord');
+    console.log('here');
+    player.playChord(chord);
+  });
+
+});
