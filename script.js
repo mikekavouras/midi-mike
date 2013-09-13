146ecaf2675ac9141370dee51e93f4897267f3fa
mikekavouras <mike@wantful.com>
2013-09-12 21:43:58 -0400

    different wave types

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function AudioPlayer() {
   this.context = new webkitAudioContext();
+  this.waveTypes = {
+    "sine" : 0,
+    "square" : 1,
+    "sawtooth" : 2,
+    "triangle" : 3
+  }
+
+  this.waveType = 0;
 
   this.init();
 }
@@ -12,6 +20,7 @@ AudioPlayer.prototype = {
     var source = this.context.createOscillator();
     var volume = this.context.createGainNode();
     source.frequency.value = Sound.notes[octave][note].frequency;
+    source.type = this.waveType;
     source.connect(volume);
     volume.connect(this.context.destination);
     source.noteOn(0);
@@ -20,12 +29,9 @@ AudioPlayer.prototype = {
       volume.gain.value -= 0.03;
       if (volume.gain.value <= 0) {
         clearInterval(t);
-        source.disconnect(this.context.destination);
+        source.disconnect(0);
       }
     }, 10);
-  },
-
-  getNewSource: function() {
   }
 };
 
@@ -40,5 +46,10 @@ $(document).ready(function() {
      var octave = parseInt($self.attr('data-octave'));
      $self.toggleClass('active');
      Player.playNote(note, octave);
+   });
+
+   $('#wave-types button').bind('click', function() {
+     var type = $(this).attr('id');
+     Player.waveType = Player.waveTypes[type];
    });
 });
