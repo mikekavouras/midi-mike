d8aa0176110c5e59d8aa4f69d51c56ecba072e42
mikekavouras <mike@wantful.com>
2013-09-12 21:26:03 -0400

    refactor and a keyboard

--- script.js
+++ script.js
@@ -1,68 +1,41 @@
 function AudioPlayer() {
   this.context = new webkitAudioContext();
-  this.sources = [];
-
-  this.octave = 4;
-  this.key = 'C';
 
   this.init();
 }
 
 AudioPlayer.prototype = {
   init: function() {
-    for (var i = 0; i < 3; i++) {
-      var oscillator = this.context.createOscillator();
-      oscillator.type = 0;
-      this.sources.push(oscillator);
-    }
   },
 
-  play: function() {
-    for (var i = 0; i < 3; i++) {
-      this.sources[i].connect(this.context.destination);
-      this.sources[i].noteOn(0);
-    }
-  },
+  playNote: function(note, octave) {
+    var source = this.context.createOscillator();
+    var volume = this.context.createGainNode();
+    source.frequency.value = Sound.notes[octave][note].frequency;
+    source.connect(volume);
+    volume.connect(this.context.destination);
+    source.noteOn(0);
 
-  pause: function() {
-    for (var i = 0; i < this.sources.length; i++) {
-      this.sources[i].disconnect(this.context.destination);
-    }
-  },
-
-  playChord: function(chord) {
-    this.pause();
-
-    chord = Sound.keys[this.key][chord];
-    for (var i = 0; i < chord.length; i++) {
-      var note = chord[i];
-      this.sources[i].frequency.value = Sound.notes[note + this.octave].frequency
-    }
-
-    this.play();
-  },
-
-  playNote: function(note) {
-
+    var t = setInterval(function() {
+      volume.gain.value -= 0.01;
+      if (volume.gain.value <= 0) {
+        clearInterval(t);
+      }
+    }, 10);
   },
 
   getNewSource: function() {
   }
 };
 
-var Player;
 $(document).ready(function() {
-   Player = new AudioPlayer();
+   var Player = new AudioPlayer();
 
-  $('#notes button').bind('click', function(e) {
-    var note = $(this).attr('data-note');
-    Player.playNote(note);
-  });
-
-  $('#chords').find('button').bind('click', function(e) {
-    var chord = $(this).attr('data-chord');
-    console.log('here');
-    Player.playChord(chord);
-  });
-
+   $('.key').bind('click, touchstart', function(e) {
+     var $self = $(this);
+     var note = $self.attr('data-note');
+     var octave = parseInt($self.attr('data-octave'));
+     $self.toggleClass('active');
+     Player.playNote(note, octave);
+   });
 });
